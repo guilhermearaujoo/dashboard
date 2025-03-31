278a4e9a2df87f6e4cdfcf22e2422af69a1d0e33
guilhermearaujoo <ghma.em1@gmail.com>
2025-03-31 18:35:21 -0300

    chore: add initial styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import { ThemeProvider } from 'styled-components'
+import ResetStyles from '@/styles/reset'
+import theme from '@/styles/theme'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <ResetStyles />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
