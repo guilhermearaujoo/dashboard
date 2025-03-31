03e59daeb66b8ccf792cdf71846fc889d84347cc
guilhermearaujoo <ghma.em1@gmail.com>
2025-03-31 18:09:33 -0300

    chore: config paths for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import eslintPlugin from '@nabla/vite-plugin-eslint'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [react(), eslintPlugin(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
