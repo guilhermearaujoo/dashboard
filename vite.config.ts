18d63c51dbecdb537b9a748e6c8e1185fe0df4ff
guilhermearaujoo <ghma.em1@gmail.com>
2025-03-31 21:31:00 -0300

    chore: add dotenv and json-server

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import eslintPlugin from '@nabla/vite-plugin-eslint'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { config } from 'dotenv'
 
+config()
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), eslintPlugin(), tsconfigPaths()],
@@ -11,5 +13,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+  define: {
+    'process.env': process.env
   }
 })
